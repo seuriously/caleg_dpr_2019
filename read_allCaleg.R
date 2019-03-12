450456c5827b2e1d55329a5a65c00a1fd2f7772e
ghilmanf <seuriously@gmail.com>
2019-03-12 08:48:05 +0700

    added simple plot for each party

--- read_allCaleg.R
+++ read_allCaleg.R
@@ -1,5 +1,9 @@
 library(dplyr)
 library(readr)
+library(stringr)
+library(lubridate)
+library(png)
+library(ggpubr)
 
 setwd('/home/ghilman/kpu_crawls/caleg/profil_caleg/')
 
@@ -53,15 +57,23 @@ for (file in file_list){
 }
 list_caleg %>% group_by(id) %>% summarise(a = n()) %>% filter(a>1)
 
-list_caleg_f = list_caleg %>% select(namaKab, id, stringJenisKelamin, partai, Dapil, provinsi)
+list_caleg_f = list_caleg %>% select(namaKab, id, stringJenisKelamin, partai, Dapil, provinsi, noUrut, gelarDepan, gelarBelakang)
 colnames(list_caleg_f)[1] = 'kota_tinggal'
 profil_caleg$caleg_id = as.integer(profil_caleg$caleg_id)
 
-fulldat = list_caleg_f %>% left_join(profil_caleg, by=c('id'='caleg_id'))
+fulldat = list_caleg_f %>% left_join(profil_caleg[,-c(1,3,4,8)], by=c('id'='caleg_id'))
 fulldat = data.frame(lapply(fulldat, function(v) { tolower(v)}))
-library(lubridate)
 
-fulldat$umur = floor((Sys.Date()-dmy(fulldat$Tanggal.Lahir))/360)
+fulldat$noUrut = as.numeric(levels(fulldat$noUrut))[fulldat$noUrut]
+fulldat$Jumlah.Anak = as.numeric(levels(fulldat$Jumlah.Anak))[fulldat$Jumlah.Anak]
+fulldat %>% mutate_if(is.factor, as.character) -> fulldat
+fulldat[6573, "Tanggal.Lahir"] = '15-10-1986'
+fulldat$umur = floor(as.numeric((Sys.Date()-dmy(fulldat$Tanggal.Lahir))/360))
+#fulldat$umur = 2019 - as.numeric(paste0(19,str_sub(fulldat$Nomor.Induk.Kependudukan..NIK., start = -6, end = -5)))
+
+
+fulldat[which(fulldat$umur>100),"umur"] = fulldat[which(fulldat$umur>100),"umur"]-100
+
 
 save.image(file="caleg.RData") 
 
@@ -69,15 +81,37 @@ library(purrr)
 library(tidyr)
 library(ggplot2)
 
-fulldat$No..Urut = as.numeric(levels(fulldat$No..Urut))[fulldat$No..Urut]
-fulldat$Jumlah.Anak = as.numeric(levels(fulldat$Jumlah.Anak))[fulldat$Jumlah.Anak]
+
+fulldat %>% 
+  group_by(partai) %>% 
+  summarise(avg = mean(umur,na.rm = T), min_age = min(umur, na.rm = T), max_age = max(umur, na.rm = T), n_tot = n(), n_na= sum(is.na(umur)))
+
+fulldat$umur = cut(fulldat$umur, breaks = 10, dig.lab = 2)
 
 fulldat %>%
-  select(stringJenisKelamin, partai, No..Urut, Agama, Status.Perkawinan, Jumlah.Anak, Pendidikan, Status.Khusus, umur) %>%
+  select(stringJenisKelamin, partai, noUrut, Agama, Status.Perkawinan, Jumlah.Anak, Pendidikan, Status.Khusus, umur) %>%
   gather() %>% 
   ggplot(aes(value)) +
   facet_wrap(~ key, scales = "free") +
   geom_histogram(stat="count")+
   theme_light()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size=6))
+
+for(pty in unique(fulldat$partai)){
+  df = filter(fulldat, partai==pty)
+  df = df %>%
+    select(stringJenisKelamin, noUrut, Agama, Status.Perkawinan, Jumlah.Anak, Pendidikan, Status.Khusus, umur) %>%
+    mutate(is_na = if_else(is.na(umur), 1, 0)) %>%
+    gather()
+    png(paste0('pict/',pty, '.png'), width = 1200, height = 868)
+    print(ggplot(df, aes(value)) +
+      facet_wrap(~ key, scales = "free") +
+      #background_image(readPNG(paste0('../bendera/', pty, '.png')))+
+      geom_histogram(stat = 'count')+
+      theme_light()+
+      theme(plot.title = element_text(size = 40, face = "bold"), axis.text.x = element_text(angle = 45, hjust = 1, size=12))+
+      labs( title= paste("Partai", toupper(pty)), y="Total Caleg", x = "")
+    )
+    dev.off()
+}
 
